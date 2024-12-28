871fd02ed77b671e5d0e6a1931a201f791b50fd9
Christian Oertlin <c.oertlin@gmail.com>
2024-12-28 15:23:01 +0100

    chore: hide home image on smaller screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,17 @@ import ImageComponent from '../components/Image/Image';
 import welcome_main_image from "../assets/welcome_main.jpg";
 function Home() {
   return (
-    <div className="min-h-screen flex flew-col sm:flex-row">
-      <div className="min-h-screen flex-1 items-center justify-center border-r border-black border">
+    <div className="flex h-screen">
+      <div className="w-full sm:w-1/3 flex flex-col items-center justify-center border-r border-black">
         <div className="p-4">
           <h1 className="text-5xl font-bold text-center font-sans text-red-600 mt-4">Hi</h1>
           <p className="text-xl text-center text-gray-700 mt-2 mb-4">Feel free to wander!</p>
         </div>
       </div>
-      <div className="min-h-screen flex-[3]">
-        <ImageComponent ImageSource={welcome_main_image} alt="image" className="w-full h-full object-cover" />
+      <div className="w-full sm:w-2/3 h-full overflow-x-auto hidden sm:block">
+        <div className="min-w-full h-full">
+          <ImageComponent ImageSource={welcome_main_image} alt="image" className="w-full h-full object-cover" />
+        </div>
       </div>
     </div>
   );
